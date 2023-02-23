d0504b9bdc90e79411d6a5637de95dd812fecca0
parmeet2 <parmeet@damacloid.com>
2023-02-22 20:27:46 -0500

    Collect Form Information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,6 +12,7 @@ const Contact = () => {
                     <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Have questions or need assistance? Contact us today and our friendly team will be happy to help.</p>
                 </div>
                     <form name='contact' method="POST" netlify>
+                    <input type="hidden" name="form-name" value="contact" />.
                     <div class="lg:w-1/2 md:w-2/3 mx-auto">
                         <div class="flex flex-wrap -m-2">
                             <div class="p-2 w-1/2">
