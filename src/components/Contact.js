158802175b63f15ba9647296a797e625f7885bd6
parmeet2 <parmeet@damacloid.com>
2023-02-24 15:21:51 -0500

    Change Button Text to Submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
                                 </div>
                             </div>
                             <div class="p-2 w-full">
-                                <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                                <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
                             </div>
                         <ContactSection/>
                         </div>
